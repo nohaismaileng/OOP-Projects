6cc6dcfeb5da32c547d40e3cf1c2878d028cfb8d
nohaismaileng <noha.ismail.eng@gmail.com>
2026-09-11 12:37:12 +0300

    Implement page object methods for item management

---
+++ page-object/page_object.cpp
@@ -0,0 +1,37 @@
+#include "page_object.h"
+
+void Page_Object::add_Item(){
+    std::cout << "Item added to page object" << endl;
+}
+
+void Page_Object::remove_Item(){
+    std::cout << "Item removed to page object" << endl;
+}
+
+void Page_Object::delete_Item(){
+    std::cout << "Item deleted to page object" << endl;
+}
+
+void page::add_Item(){
+    std::cout << "Item added to Page" << endl;
+}
+
+void page::remove_Item(){
+    std::cout << "Item removed to Page" << endl;
+}
+
+void page::delete_Item(){
+    std::cout << "Item deleted to Page" << endl;
+}
+
+void Copy::add_Item(){
+    std::cout << "Item added to Page" << endl;
+}
+
+void Copy::remove_Item(){
+    std::cout << "Item removed to Page" << endl;
+}
+
+void Copy::delete_Item(){
+    std::cout << "Item deleted to Page" << endl;
+}
